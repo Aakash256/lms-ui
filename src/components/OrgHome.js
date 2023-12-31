69b619b6003a25232f571234f4f103a20b7e871a
Feya Shah <feya.shah@askpinkypromise.com>
2023-12-31 10:58:50 +0530

    Adding logic to have create organization button

--- src/components/OrgHome.js
+++ src/components/OrgHome.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Table } from 'react-bootstrap';
-import {useLocation} from 'react-router-dom';
+import { Container, Button } from 'react-bootstrap';
+import { useLocation, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import "./Home.css";
 
 const OrgHome = () => {
     const [organization, setOrganization] = useState();
-    const [loading, setLoading] = useState(true);
     const location = useLocation();
+    const navigate = useNavigate();
 
   useEffect(() => {
     const fetchUserMe = async () => {
@@ -30,15 +30,16 @@ const OrgHome = () => {
 
         const orgId = user?.orgId;
 
-        const orgResponse = await axios.get("http://localhost:8090/v1/organization/" + orgId, {
-            headers: {
-            Authorization: 'Bearer ' + accessToken, // Replace YOUR_ACCESS_TOKEN with the actual token
-            },
-        });
-
-        console.log("org response", orgResponse.data);
-        setOrganization(orgResponse.data);
-        setLoading(false);
+        if(orgId) {
+            const orgResponse = await axios.get("http://localhost:8090/v1/organization/" + orgId, {
+                headers: {
+                Authorization: 'Bearer ' + accessToken, // Replace YOUR_ACCESS_TOKEN with the actual token
+                },
+            });
+    
+            console.log("org response", orgResponse.data);
+            setOrganization(orgResponse.data);
+        }
 
         // Handle the response data as needed
       } catch (error) {
@@ -50,37 +51,43 @@ const OrgHome = () => {
     fetchUserMe();
   }, []);
 
+  const handleCreateOrganization = () => {
+    // Navigate to the create organization page
+    navigate('/organization/create');
+  };
+
   return (
     <Container className="text-center home-container">
-        <div className="box-container text-center" style={{ border: '1px solid #ccc', borderRadius: '10px', padding: '20px', maxWidth: '600px', width: '100%' }}>
+      <div className="box-container text-center" style={{ border: '1px solid #ccc', borderRadius: '10px', padding: '20px', maxWidth: '600px', width: '100%' }}>
         <h2>Organization Details</h2>
-            {loading ? (
-                <p>Loading...</p>
-            ) : (
-            <Table striped bordered hover>
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Description</th>
-                <th>Phone Number</th>
-                <th>Status</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>{organization.id}</td>
-                <td>{organization.name}</td>
-                <td>{organization.email}</td>
-                <td>{organization.description}</td>
-                <td>{organization.phoneNo}</td>
-                <td>{organization.status}</td>
-              </tr>
-            </tbody>
-          </Table>
-          )}
-        </div>
+        {organization ? (
+          <div>
+            <div>
+              <strong>ID:</strong> {organization.id}
+            </div>
+            <div>
+              <strong>Name:</strong> {organization.name}
+            </div>
+            <div>
+              <strong>Email:</strong> {organization.email}
+            </div>
+            <div>
+              <strong>Description:</strong> {organization.description}
+            </div>
+            <div>
+              <strong>Phone Number:</strong> {organization.phoneNo}
+            </div>
+            <div>
+              <strong>Status:</strong> {organization.status}
+            </div>
+            </div>
+        ): 
+            (
+              <Button variant="primary" onClick={handleCreateOrganization}>
+                Create Organization
+              </Button>
+            )}
+          </div>
     </Container>
   );
 };
