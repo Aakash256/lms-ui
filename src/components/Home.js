6f93be86718c8f6a0086be484187cedece0bd476
Feya Shah <feya.shah@askpinkypromise.com>
2024-01-27 09:34:07 +0530

    Responsive design for home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,22 +6,26 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 const Home = () => {
   return (
-    <div className="d-flex align-items-center justify-content-center text-center min-vh-100">
-      <div className="mx-auto border border-1 border-secondary rounded p-4 w-50">
+    <Container className="d-flex align-items-center justify-content-center text-center min-vh-100">
+      <div className="mx-auto border border-1 border-secondary rounded p-4 w-100 w-md-50">
         <h2>Lead Management System</h2>
         <p>The Lead Management System provides businesses with an efficient platform to streamline and organize their 
           leads, offering a centralized space for comprehensive lead management, fostering enhanced organization and 
           productivity.</p>
-        <div className="row text-center justify-content-center">
-          <Link to="/signup" className="col-md-4">
-            <Button className="px-5 btn btn-success">Sign Up</Button>
-          </Link>
-          <Link to="/login" className="col-md-4">
-            <Button className="px-5 btn btn-success">Login</Button>
-          </Link>
-        </div>
+        <Row className="text-center justify-content-center">
+          <Col md={4} className="mb-3">
+            <Link to="/signup">
+              <Button className="px-4 btn btn-success">Sign Up</Button>
+            </Link>
+          </Col>
+          <Col md={4} className="mb-3">
+            <Link to="/login">
+              <Button className="px-4 btn btn-success">Login</Button>
+            </Link>
+          </Col>
+        </Row>
       </div>
-    </div>
+    </Container>
   );
 };
 
